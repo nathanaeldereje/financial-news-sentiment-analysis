0a8ffb014c6cf59c72402f55b743e696e93bd15a
Nathanael Dereje <151948534+nathanaeldereje@users.noreply.github.com>
2025-11-22 10:09:19 +0300

    export cleaned

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,13 +1,31 @@
 import pandas as pd
 
-def load_and_preprocess_data(path="../data/raw_analyst_ratings.csv") -> pd.DataFrame:
+def load_data(path) -> pd.DataFrame:
     """
-    Load the raw Benzinga analyst ratings dataset and perform initial preprocessing.
+    Load the raw Benzinga analyst ratings dataset.
     
     Parameters
     ----------
-    path : str, default "../data/raw_analyst_ratings.csv"
+    path : str, 
         Path to the CSV file.
+    
+    Returns
+    -------
+    pd.DataFrame
+        - normalized UTC date (naive timestamp)
+    """
+    df = pd.read_csv(path, parse_dates=["date"], index_col=0)
+    # Handle timezone-aware timestamps properly
+    df['date'] = pd.to_datetime(df['date'],format='mixed', utc=True)
+    df['date'] = df['date'].dt.tz_convert('UTC').dt.tz_localize(None)
+    return df
+
+def preprocess_data(df):
+    """
+    perform initial preprocessing. 
+    Parameters
+    ----------
+    pd.DataFrame
     
     Returns
     -------
@@ -15,14 +33,7 @@ def load_and_preprocess_data(path="../data/raw_analyst_ratings.csv") -> pd.DataF
         Cleaned dataframe with additional features:
         - headline_length
         - year, weekday, hour
-        - normalized UTC date (naive timestamp)
     """
-    df = pd.read_csv(path, parse_dates=["date"], index_col=0)
-    
-    # Handle timezone-aware timestamps properly
-    df['date'] = pd.to_datetime(df['date'],format='mixed', utc=True)
-    df['date'] = df['date'].dt.tz_convert('UTC').dt.tz_localize(None)
-    
     # Feature engineering
     df['headline_length'] = df['headline'].astype(str).apply(len)
     df['year'] = df['date'].dt.year
@@ -30,5 +41,4 @@ def load_and_preprocess_data(path="../data/raw_analyst_ratings.csv") -> pd.DataF
     df['weekday'] = df['date'].dt.day_name()
     df['hour'] = df['date'].dt.hour
     df['date_only'] = df['date'].dt.date
-    
     return df
